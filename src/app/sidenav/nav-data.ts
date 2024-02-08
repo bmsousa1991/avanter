59dea06f6cd1c825e919853c704867efcf8e9abc
bmsousa1991@gmail.com <bmsousa1991@gmail.com>
2024-02-07 23:12:13 -0300

    Projeto Inicial

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,8 @@
+export const navbarData = [
+  {
+    routeLink: 'dasboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  }
+
+];
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+
+  collapsed = false;
+  navData = navbarData;
+
+}
