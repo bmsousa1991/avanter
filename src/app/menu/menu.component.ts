6c332256a08817544d0415a49ff4b76e6ad70331
bmsousa1991@gmail.com <bmsousa1991@gmail.com>
2024-02-09 15:42:23 -0300

    menu nav

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input,} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent implements OnInit{
-  constructor() {}
-  ngOnInit(): void {
+export class MenuComponent {
 
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
+
+  getMenuClass(): string {
+
+    let styleClass = '';
+
+    if ( this.collapsed && this.screenWidth > 768) {
+      styleClass = 'menu-trimmed';
+    } else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'menu-my-screen'
+    }
+    return styleClass;
   }
+
 }
+
--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -3,6 +3,18 @@ export const navbarData = [
     routeLink: 'dashboard',
     icon: 'fal fa-home',
     label: 'Dashboard'
-  }
+  },
+
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Chamados'
+  },
+
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Clientes'
+  },
 
 ];
